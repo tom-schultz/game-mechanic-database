1d15952ff2d6b5c7411d6c07cfbbda6a3498c2c1
Tom Schultz <2112416+tom-schultz@users.noreply.github.com>
2023-07-03 01:31:17 -0400

    Mechanics done, needs UI!

--- Scenes/BobberFishing.gd
+++ Scenes/BobberFishing.gd
@@ -1,10 +1,15 @@
-extends Sprite2D
+extends Node2D
+
+var bobber
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	bobber = get_node("Bobber")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position = 5 * 
+	pass
+
+func _unhandled_key_input(event):
+	if (event.is_pressed() and event.keycode == KEY_SPACE):
+		bobber.hit()
