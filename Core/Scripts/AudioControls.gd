d65a9e5c4469a977c755bf3fab44f1ab41f47beb
Tom Schultz <2112416+tom-schultz@users.noreply.github.com>
2023-09-16 23:29:06 -0400

    Actual playlist and track system, plus new music!

--- Core/Scripts/AudioControls.gd
+++ Core/Scripts/AudioControls.gd
@@ -3,8 +3,14 @@ extends GridContainer
 const MUSIC_VOL_DB_KEY = "MUSIC_VOLUME"
 const SFX_VOL_DB_KEY = "SFX_VOLUME"
 
+@export var playlist_label : Label
+@export var track_label : Label
+
 @onready var _music_slider : HSlider = find_child("Music Volume Slider")
 @onready var _sfx_slider : HSlider = find_child("SFX Volume Slider")
+
+var _playlists : Array
+var _playlist_index = 0
 
 func _ready():
 	if LocalDb.has_key(MUSIC_VOL_DB_KEY):
@@ -16,6 +22,9 @@ func _ready():
 		var vol = LocalDb.read_key(SFX_VOL_DB_KEY)
 		AudioPlayer.set_sfx_volume(vol)
 		_sfx_slider.set_value_no_signal(vol)
+	
+	_playlists = AudioPlayer.get_playlists()
+	_update_playlist()
 
 func _on_music_volume_slider_value_changed(value):
 	LocalDb.write_key(MUSIC_VOL_DB_KEY, value)
@@ -27,3 +36,24 @@ func _on_sfx_volume_slider_value_changed(value):
 
 func _on_button_pressed():
 	AudioPlayer.play_button_press_sfx()
+
+func _on_previous_playlist_pressed():
+	_playlist_index = wrapi(_playlist_index + 1, 0, _playlists.size())
+	_update_playlist()
+
+func _on_next_playlist_pressed():
+	_playlist_index = wrapi(_playlist_index - 1, 0, _playlists.size())
+	_update_playlist()
+
+func _update_playlist():
+	playlist_label.text = _playlists[_playlist_index]
+	AudioPlayer.set_playlist(_playlists[_playlist_index])
+	track_label.text = AudioPlayer.track_name()
+
+func _on_previous_track_pressed():
+	AudioPlayer.change_track(-1)
+	track_label.text = AudioPlayer.track_name()
+
+func _on_next_track_pressed():
+	AudioPlayer.change_track(1)
+	track_label.text = AudioPlayer.track_name()
--- Core/Scripts/AudioPlayer.gd
+++ Core/Scripts/AudioPlayer.gd
@@ -1,22 +1,34 @@
 extends Node
 
 const VOLUME_MIN = -40
+const playlist_prefix = "res://Shared/Audio/Music"
 
 @export var _button_press_sfx : AudioStream
 @export_range(0,1) var _button_press_volume : float = 1
 @export var _tab_click_sfx : AudioStream
 @export_range(0,1) var _tab_click_volume : float = 1
+@export var playlists : Dictionary
 
-@onready var _music_blue : AudioStreamPlayer = get_node("Music Player Blue")
+@onready var _music_player : AudioStreamPlayer = get_node("Music Player")
 @onready var _sfx_player : AudioStreamPlayer = get_node("SFX Player")
 
 var _music_volume : float = 0
 var _sfx_volume : float = 0
+var _track_index : int = 0
+var _tracks
+
+func set_playlist(playlist_name : String):
+	_tracks = playlists[playlist_name]
+	_track_index = 0
+	_play_current_track()
+	
+func get_playlists():
+	return playlists.keys()
 
 func set_music_volume(volume : float):
 	var remapped = remap(volume, 0, 100, VOLUME_MIN, 0)
 	_music_volume = remapped
-	_music_blue.volume_db = remapped
+	_music_player.volume_db = remapped
 
 func set_sfx_volume(volume : float):
 	var remapped = remap(volume, 0, 100, VOLUME_MIN, 0)
@@ -32,3 +44,19 @@ func play_sfx(stream : AudioStream, volume_scale : float = 1):
 	_sfx_player.volume_db = _sfx_volume + (1 - volume_scale) * VOLUME_MIN
 	_sfx_player.stream = stream
 	_sfx_player.play()
+	
+func change_track(delta : int):
+	_track_index = wrapi(_track_index + delta, 0, _tracks.size())
+	_play_current_track()
+
+func _play_current_track():
+	_music_player.stream = _tracks[_track_index]
+	_music_player.play()
+
+func _on_music_player_finished():
+	change_track(1)
+
+func track_name():
+	var filename = _tracks[_track_index].resource_path.get_file()
+	var ext = filename.get_extension()
+	return filename.replace("." + ext, "")
